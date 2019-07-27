8ae1abfe469a95fb57a287631a1d7b39b468d837
cfstras <cfstras@users.noreply.github.com>
2019-07-27 22:07:34 +0200

    add setup, readme and systemd services

--- README.md
+++ README.md
@@ -1,13 +1,32 @@
 # github-ssh-auth
 
+Automatically provision SSH authorized_keys to your user by scraping a GitHub organisation (optional team).
 
-## Example requests
+## Setup
+- If you're on Debian, you will need some more packages:
+```bash
+apt install python3
+```
+
+- Enable lingering services for your user:
+```bash
+sudo loginctl enable-linger $USER
+```
+
+The following script will:
+
+- Make a virtualenv and install dependencies from [requirements.txt](requirements.txt)
+- Setup a systemd-user-service `github-ssh-auth.service`
+- Setup a timer which refreshes authorized_keys every 30 minutes
 
 ```bash
-
-http https://api.github.com/orgs/ennexos/teams/$TEAM_NAME Authorization:"Bearer $TOKEN"
-
-TEAM=(team id from above)
-
-http https://api.github.com/teams/$TEAM/members Authorization:"Bearer $TOKEN"
+./setup.sh
 ```
+
+- Edit `config.yml`
+  - Create an access token
+  - Set your organization and team
+  - optionally, add fallback ssh keys which will always be added
+
+# License
+GPLv3. Other licenses available upon request.
---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -euo pipefail
+
+SERVICE_PATH=$HOME/.config/systemd/user/
+SERVICE=github-ssh-auth.service
+TIMER=github-ssh-auth.timer
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+ENV=.env
+PIP=$ENV/bin/pip
+
+make_venv() {
+    echo "### Making virtualenv"
+    [[ -x $PIP ]] || python3 -m venv $ENV
+    $PIP install -r requirements.txt
+}
+
+install_service() {
+    echo "### Installing systemd timer"
+    mkdir -p "$SERVICE_PATH"
+
+    sed "s#DIR#${DIR}#g" "$SERVICE" > "$SERVICE_PATH/$SERVICE"
+    sed "s#DIR#${DIR}#g" "$TIMER" > "$SERVICE_PATH/$TIMER"
+
+    echo "### Enabling systemd timer"
+    systemctl --user daemon-reload
+    systemctl --user reenable --now "$TIMER"
+
+    echo "### Starting service once"
+    systemctl --user start "$SERVICE"
+    echo "### Please edit config.yml to your liking."
+}
+
+check_linger() {
+    if loginctl show-user "$USER" | grep Linger=no >/dev/null 2>&1; then
+        echo "Enabling loginctl linger"
+        sudo loginctl enable-linger "$USER"
+    fi
+}
+
+make_venv
+check_linger
+install_service
